5c6b33fe5628d6a53546f486efa6ed8b41d25946
Theozzean <marcelotheodoro230301@gmail.com>
2024-09-29 20:38:01 -0300

    novos estudos

---
+++ condicionais/if.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 10;
+$b = 21;
+
+if ($a > $b) {
+    echo "Realmente A é maior que B <br>";
+} elseif ($b == 20) {
+    echo "B é igual a 20!";
+} else {
+    echo "Nenhuma condicao é verdadeira";
+}
+
+
